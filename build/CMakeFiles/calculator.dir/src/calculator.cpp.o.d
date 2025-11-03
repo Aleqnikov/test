c16365a44532d0f0208df92ea8e9c296bbe3d186
Алейников Иван Алексеевич <134135033+Aleqnikov@users.noreply.github.com>
2025-11-03 14:32:57 +0300

    Delete build directory

--- build/CMakeFiles/calculator.dir/src/calculator.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/calculator.dir/src/calculator.cpp.o: \
- /home/ivan/Documents/tests/test/src/calculator.cpp \
- /usr/include/stdc-predef.h \
- /home/ivan/Documents/tests/test/src/calculator.h
